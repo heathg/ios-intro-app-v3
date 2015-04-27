496b4ada09c0338eef5f46195aeb82b580ce3eaa
heathg <Heathglover@Heaths-MacBook-Pro.local>
2015-04-27 21:22:12 +1000

    Implementing an App Complete
    Tutorial: Add Data Completed and compiled successfully

--- ToDoList/ToDoList/ToDoItem.m
+++ ToDoList/ToDoList/ToDoItem.m
@@ -1,18 +1,13 @@
 //
-//  AddToDoItemViewController.h
+//  ToDoItem.m
 //  ToDoList
 //
 //  Created by Heath Glover on 27/04/2015.
 //  Copyright (c) 2015 HeathG. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import "ToDoItem.h"
 
-@interface AddToDoItemViewController : UIViewController
-
-@property ToDoItem *toDoItem;
+@implementation ToDoItem
 
 @end
-
-
